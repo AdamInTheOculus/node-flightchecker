29bdd386bcf6c61ea24ba4a88ccbf9c9bf0e456e
Adam Sinclair <adamsinclair@Adams-MacBook-Air.local>
2016-10-19 01:30:24 -0400

    created initial project

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to get started
+- Fork this repo
+- Edit your `API Key` in app.js
+- Edit any flight parameters in `trip` object
+- Enter: ``` node app.js ``` in project directory
---
+++ app.js
@@ -0,0 +1,80 @@
+/*
+ * Project: Flight Checker
+ * Author: Adam Sinclair
+ * Date: October 19th, 2016
+ * Purpose: Automatically check flight prices for specific destination at a specific time.
+ */
+
+// Use documentation below for all response data
+// https://developers.google.com/qpx-express/v1/trips/search
+
+var https = require('https');
+
+// Google API Key
+var key = '';
+
+// Trip Information
+var trip = {
+	origin: 'YYZ',
+	destination: 'AMS',
+	date: '2017-06-21'
+};
+
+// Request Options
+var post_options = {
+	host: 'www.googleapis.com',
+	path: '/qpxExpress/v1/trips/search?key=' + key,
+	method: 'POST',
+	json: true,
+	headers: {
+		'content-type': 'application/json'
+	}
+};
+
+// Request Body
+var post_body = JSON.stringify({
+  "request": {
+    "passengers": {
+      "adultCount": "1"
+    },
+    "slice": [
+      {
+        "origin": trip.origin,
+        "destination": trip.destination,
+        "date": trip.date
+      }
+    ],
+    "solutions": "1"
+  }
+});
+
+// Set up the request
+var result = '';
+var post_req = https.request(post_options, function(res) {
+	res.setEncoding('utf8');
+
+	res.on('data', function(chunk) {
+		result += chunk;
+	});
+
+	res.on('end', function() {
+		// Convert string data back to JSON
+		console.log(result);
+		var jsonData = JSON.parse(result);
+		console.log('-----------------------');
+		console.log('Origin: \t' + jsonData.trips.data.airport[1].name + ' - ('+jsonData.trips.data.airport[1].code+')');
+		console.log('Destination: \t' + jsonData.trips.data.airport[0].name + ' - ('+jsonData.trips.data.airport[0].code+')');
+		console.log('Depart. Time:   ' + jsonData.trips.tripOption[0].slice[0].segment[0].leg[0].departureTime);
+		console.log('Depart. Term:   ' + jsonData.trips.tripOption[0].slice[0].segment[0].leg[0].originTerminal);
+		console.log('Arrival Time: \t' + jsonData.trips.tripOption[0].slice[0].segment[0].leg[0].arrivalTime);
+		console.log('Duration: \t' + jsonData.trips.tripOption[0].slice[0].segment[0].leg[0].duration + ' minutes');
+		console.log('Aircraft Type: \t' + jsonData.trips.data.aircraft[0].name);
+		console.log('Carrier Name: \t' + jsonData.trips.data.carrier[0].name);
+		console.log('Sale Total: \t' + jsonData.trips.tripOption[0].saleTotal);
+		console.log('-----------------------');
+	});
+
+});
+
+// Post the data
+post_req.end(post_body);
