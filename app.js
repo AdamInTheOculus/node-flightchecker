c3ae34e3bee489be1edb94edfdfde6f8ea5c266a
Adam Sinclair <adamsinclair@Adams-MacBook-Air.local>
2016-10-19 13:11:19 -0400

    cleaned up unnecessary variables

--- app.js
+++ app.js
@@ -9,12 +9,7 @@
 // https://developers.google.com/qpx-express/v1/trips/search
 
 var https = require('https');
-var credentials = require('./credentials.js');
-
-console.log(credentials);
-
-// Google API Key
-var key = credentials.key;
+var _credentials = require('./credentials.js').key;
 
 // Trip Information
 var trip = {
@@ -26,7 +21,7 @@ var trip = {
 // Request Options
 var post_options = {
 	host: 'www.googleapis.com',
-	path: '/qpxExpress/v1/trips/search?key=' + key,
+	path: '/qpxExpress/v1/trips/search?key=' + _credentials,
 	method: 'POST',
 	json: true,
 	headers: {
@@ -52,21 +47,24 @@ var post_body = JSON.stringify({
 });
 
 // Set up the request
-var result = '';
 var post_req = https.request(post_options, function(res) {
-	res.setEncoding('utf8');
 
-	res.on('data', function(chunk) {
-		result += chunk;
-	});
+	// Variable to hold response data
+	var result = '';
 
+	// Display any errors that occur
 	res.on('error', function(err) {
 		console.log(err);
 	});
 
+	// Append all data to variable
+	res.on('data', function(chunk) {
+		result += chunk;
+	});
+
+	// Convert data to JSON and display to console.
 	res.on('end', function() {
 		// Convert string data back to JSON
-		console.log(result);
 		var jsonData = JSON.parse(result);
 		console.log('-----------------------');
 		console.log('Origin: \t' + jsonData.trips.data.airport[1].name + ' - ('+jsonData.trips.data.airport[1].code+')');
@@ -74,13 +72,19 @@ var post_req = https.request(post_options, function(res) {
 		console.log('Depart. Time:   ' + jsonData.trips.tripOption[0].slice[0].segment[0].leg[0].departureTime);
 		console.log('Depart. Term:   ' + jsonData.trips.tripOption[0].slice[0].segment[0].leg[0].originTerminal);
 		console.log('Arrival Time: \t' + jsonData.trips.tripOption[0].slice[0].segment[0].leg[0].arrivalTime);
+		console.log('Sale Total: \t' + jsonData.trips.tripOption[0].saleTotal);
+		console.log('Refundable?: \t' + jsonData.trips.tripOption[0].pricing[0].refundable);
+		console.log('-----------------------');
 		console.log('Duration: \t' + jsonData.trips.tripOption[0].slice[0].segment[0].leg[0].duration + ' minutes');
+		console.log('Mileage: \t' + jsonData.trips.tripOption[0].slice[0].segment[0].leg[0].mileage + ' miles');
 		console.log('Aircraft Type: \t' + jsonData.trips.data.aircraft[0].name);
 		console.log('Carrier Name: \t' + jsonData.trips.data.carrier[0].name);
-		console.log('Sale Total: \t' + jsonData.trips.tripOption[0].saleTotal);
+		console.log('Flight #: \t' + jsonData.trips.tripOption[0].slice[0].segment[0].flight.number);
+		console.log('Cabin: \t\t' + jsonData.trips.tripOption[0].slice[0].segment[0].cabin);
+		console.log('Booking Code: \t' + jsonData.trips.tripOption[0].slice[0].segment[0].bookingCode);
+		console.log('Avail. Seats:   ' + jsonData.trips.tripOption[0].slice[0].segment[0].bookingCodeCount);
 		console.log('-----------------------');
 	});
-
 });
 
 // Post the data
